d2515c4ed7a10c6ed19203c4cc595200e33e5789
Marcin Jaszczuk <martim0t0@gmail.com>
2021-05-23 20:29:55 -0500

    add card to Home Page and About Page with content that will need to be changed.

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Aboot</h5>
+
+  <q-page class="flex-center flex-center constrain">
+    <q-card
+      bordered
+      class="my-card bg-teal-2 q-mb-md"
+    >
+      <q-card-section>
+        <div class="text-h4" style="opacity:.5">
+          Who am I?
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+<!-- {         {post.content }} -->
+      </q-card-section>
+
+
+      <q-card-section class="q-pt-none">
+        <!-- :src="post.imageUrl" -->
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-section class="text-caption">
+        <!-- {{ post.date }} -->
+      </q-card-section>
+    </q-card>
+  </q-page>
+
+
   </q-page>
 </template>
 
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+
+  <q-page class="flex-center flex-center constrain">
+    <q-card
+      bordered
+      class="my-card bg-teal-2 q-mb-md"
+    >
+      <q-card-section>
+        <div class="text-h4" style="opacity:.5">
+          Welcome to my page!
+        </div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+<!-- {         {post.content }} -->
+      </q-card-section>
+
+
+      <q-card-section class="q-pt-none">
+        <!-- :src="post.imageUrl" -->
+      </q-card-section>
+
+      <q-separator inset />
+
+      <q-card-section class="text-caption">
+        <!-- {{ post.date }} -->
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
